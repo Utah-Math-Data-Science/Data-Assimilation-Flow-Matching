50e7c391b4a60677efd0ae44a73f72dc465acfb8
Taos Transue <spacechips@rocketmail.com>
2025-02-08 19:05:20 +0000

    integrating configuration

--- src/conf/conf.py
+++ src/conf/conf.py
@@ -1,10 +1,13 @@
+from typing import List, Any
 from dataclasses import field
 from pathlib import Path
 
 import sqlalchemy as sa
+import omegaconf
 import hydra_orm.utils
 from hydra_orm import orm
 
+from conf import datasets, models
 from dafm import utils
 
 
@@ -13,20 +16,23 @@ def get_engine(dir=str(utils.DIR_ROOT), name='runs'):
 
 
 class Conf(orm.Table):
+    defaults: List[Any] = hydra_orm.utils.make_defaults_list([
+        dict(dataset=omegaconf.MISSING),
+        dict(model=omegaconf.MISSING),
+        '_self_',
+    ])
     root_dir: str = field(default=str(utils.DIR_ROOT.resolve()))
     out_dir: str = field(default=str((utils.DIR_ROOT/'..'/'..'/'out'/'dafm').resolve()))
     run_subdir: str = field(default='runs')
-    prediction_filename: str = field(default='prediction.pt')
+    prediction_filename: str = field(default='trajectories.parquet')
     device: str = field(default='cuda')
 
     alt_id: str = orm.make_field(orm.ColumnRequired(sa.String(8), index=True, unique=True), init=False, omegaconf_ignore=True)
     rng_seed: int = orm.make_field(orm.ColumnRequired(sa.Integer), default=42)
     fit: bool = orm.make_field(orm.ColumnRequired(sa.Boolean), default=True)
 
-    time_step_count: int = orm.make_field(orm.ColumnRequired(sa.Integer), default=100)
-    epoch_count: int = orm.make_field(orm.ColumnRequired(sa.Integer), default=600)
-    batch_size: int = orm.make_field(orm.ColumnRequired(sa.Integer), default=1000)
-    shuffle_training_samples: bool = orm.make_field(orm.ColumnRequired(sa.Boolean), default=True)
+    dataset = orm.OneToManyField(datasets.Dataset, required=True, default=omegaconf.MISSING)
+    model = orm.OneToManyField(models.Model, required=True, default=omegaconf.MISSING)
 
     @property
     def run_dir(self):
@@ -39,3 +45,6 @@ sa.event.listens_for(Conf, 'before_insert')(
 
 
 orm.store_config(Conf)
+orm.store_config(datasets.DoubleWell, group=Conf.dataset.key)
+orm.store_config(models.ScoreMatching, group=Conf.model.key)
+orm.store_config(models.FlowMatching, group=Conf.model.key)
